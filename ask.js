32546f3401b68500bae757cf179e33bd2fa53c1a
Your Name <you@example.com>
2021-09-04 16:56:11 +0800

    add ask

---
+++ ask.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+    type: 'text',
+    name: '项目名称',
+    message: '请输入项目名称',
+    default:'vite-project'
+   },
+   {
+    type: 'text',
+    name: '版本号',
+    message: '请输入版本号',
+    default:'0.0.0'
+   },
+   {
+    type: 'text',
+    name: '作者',
+    message: '请输入项目作者',
+    default:'zhangrenyang'
+   }
+]
